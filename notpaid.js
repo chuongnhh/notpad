df915dce84b0a22c1e62584619286559d6d5c843
Nguyen Hoang Chuong <chuongnh.hcm@gmail.com>
2019-09-10 20:51:11 +0700

    Update notpaid.js

--- notpaid.js
+++ notpaid.js
@@ -1,7 +1,7 @@
  // Next time use letsdeel.com to make sure you get paid
         (function () {
             /* change these variables as you wish */
-            var due_date = new Date('2019-01-10');
+            var due_date = new Date('2019-09-10');
             var days_deadline = 15;
             /* stop changing here */
 
